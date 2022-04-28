b3116189f179ee3a62cedb31b27dc3130b049e4f
jennifermcneil <mcn21005@byui.edu>
2022-04-27 19:01:02 -0700

    lesson 2 changes

--- lesson2/js/currentdate.js
+++ lesson2/js/currentdate.js
@@ -1,7 +1,5 @@
 const d = new Date();
 const year = d.getFullYear();
 
-// using getElementById
 document.getElementById('currentYear').textContent = year;
 document.querySelector('#lastUpdated').textContent = document.lastModified;
-
