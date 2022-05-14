7a37ef4211e8a6ca1b45e89802a53018bc3541cf
Jacksonckc <jackson858216047@gmail.com>
2022-05-13 18:18:45 -0600

    changes

---
+++ chamber/js/index.js
@@ -0,0 +1,6 @@
+// const button = document.addEventListener('click', addStyleFunction)
+
+function addStyleFunction() {
+  var element = document.querySelector('.menu')
+  element.classList.toggle('showList')
+}
