2d30de25211dd88582eee718268a908363fefc5d
jennifermcneil <mcn21005@byui.edu>
2022-05-14 10:12:54 -0700

    main

--- chamber/js/hamburgerButton.js
+++ chamber/js/hamburgerButton.js
@@ -1,6 +1,13 @@
 // Hamburger button
-const hamburgerButton = document.querySelector('.hambugerBtn');
-const miniNav = document.querySelector('.navigation');
+// const hamburgerButton = document.querySelector('.hambugerBtn');
+// const miniNav = document.querySelector('.navigation');
 
-hamburgerButton.addEventListener('click', () => {miniNav.classList.toggle('responsive')}, false);
-hamburgerButton.addEventListener('click', () => {hamburgerButton.classList.toggle('responsive')}, false);
+// hamburgerButton.addEventListener('click', () => {miniNav.classList.toggle('responsive')}, false);
+// hamburgerButton.addEventListener('click', () => {hamburgerButton.classList.toggle('responsive')}, false);
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
