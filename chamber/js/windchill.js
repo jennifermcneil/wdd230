1ca2296d93a110fbc4403f0bf62fd851faf0fb89
jennifermcneil <mcn21005@byui.edu>
2022-05-28 07:30:54 -0700

    main

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -16,5 +16,5 @@ document.querySelector('#windChill').innerHTML = windchill;
 
 // The formula to calculate the wind chill factor. The .toFixed is to round the number.
 function windChill (temp, speed) {
-  return (35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)).toFixed();
+  return (35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16)).toFixed(2);
 }
