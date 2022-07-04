f150590836f70ab8ea16f2dbbafa10b8973972ec
jennifermcneil <mcn21005@byui.edu>
2022-07-03 20:13:41 -0700

    main

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,5 +1,3 @@
-// const button = document.addEventListener('click', addStyleFunction)
-
 function addStyleFunction() {
   var element = document.querySelector('.menu')
   element.classList.toggle('showList')
